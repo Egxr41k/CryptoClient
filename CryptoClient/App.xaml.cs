5ed2a0ea5665bd7cf8a3623bcb294189737c1200
Egxr41k <egor2005krava@gmail.com>
2023-07-21 11:20:36 +0300

    create base mvvm structure

--- CryptoClient/App.xaml.cs
+++ CryptoClient/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CryptoClient.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +14,26 @@ namespace CryptoClient
     /// </summary>
     public partial class App : Application
     {
+        //private readonly SelectedListAppModelStore _selectedListAppModelStore;
+        //private readonly ModalNavigationStore _modalNavigationStore;
+        //private readonly ListAppStore _listAppStore;
+
+        public App()
+        {
+            //_listAppStore = new ListAppStore();
+            //_modalNavigationStore = new ModalNavigationStore();
+            //_selectedListAppModelStore = new SelectedListAppModelStore(_listAppStore);
+        }
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var cryptoClientViewModel = new CryptoClientViewModel();
+            MainWindow = new MainWindow()
+            {
+                DataContext = new MainViewModel(cryptoClientViewModel)
+            };
+            MainWindow.Show();
+
+            base.OnStartup(e);
+        }
     }
 }
---
+++ CryptoClient/ViewModels/MainViewModel.cs
@@ -0,0 +1,19 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoClient.ViewModels
+{
+    internal class MainViewModel : ObservableObject
+    {
+        public CryptoClientViewModel CryptoClientViewModel { get; set; }
+        public MainViewModel(CryptoClientViewModel cryptoClientViewModel)
+        {
+            CryptoClientViewModel = cryptoClientViewModel;
+        }
+
+    }
+}
